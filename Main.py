d9753cc8f9b21a0ef427e411bab92d65a0863aae
broli <carlos.ferrabone@gmail.com>
2025-05-20 11:17:04 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 #https://github.com/broli/PKB-Open-jobs-report
+#https://github.com/broli/PKB-Open-jobs-report
 # Main.py
 # from openJobs_class import OpenJobsApp # If you kept the old filename
 from app_shell import OpenJobsApp # If you renamed to app_shell.py
